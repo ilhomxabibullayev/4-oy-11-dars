8e628e76a6026890f554795345e705c8c98d317e
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-28 23:19:18 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// 3 masala
+
+const arr = [1, 2, 3, 4, 5];
+
+const newarr = arr.map(function name(c) {
+    return c ** 2;
+})
+
+console.log(newarr);
+
+// 4 masala
+
+// 5 masala
+
+const text = 'George Raymond Richard Martin';
+
+const newtext = text.split(function name(i) {
+    return i.charAt(1)
+})
+
+console.log(newtext);
+
+// 7 masala
+
+const arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newarr6 = arr6.filter(function name(g) {
+    return g % 2 == 0;
+})
+
+console.log(newarr6);
+
+const arr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newarr7 = arr6.filter(function name(g) {
+    return g % 2 !== 0;
+})
+
+console.log(newarr7);
